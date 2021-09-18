0f506fe487886d93f1cc3bc70fcb34931cb42e63
khadkakrishna <krishnakhadka2802@gmail.com>
2021-09-18 13:56:53 +0530

    Adding Config for login

---
+++ src/main/java/com/development/SpringOAuthApplication/config/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.development.SpringOAuthApplication.config;
+
+import com.development.SpringOAuthApplication.service.CustomOAuth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private CustomOAuth2UserService userService;
+
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .httpBasic()
+                .and().csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(userService);
+    }
+}
