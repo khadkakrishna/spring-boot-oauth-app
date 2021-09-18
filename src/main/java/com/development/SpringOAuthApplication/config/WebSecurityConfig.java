84d445a488be3d71fb0b63036d19f4aca5b6a513
khadkakrishna <krishnakhadka2802@gmail.com>
2021-09-18 15:22:17 +0530

    Removing imports that are not required

--- src/main/java/com/development/SpringOAuthApplication/config/WebSecurityConfig.java
+++ src/main/java/com/development/SpringOAuthApplication/config/WebSecurityConfig.java
@@ -2,14 +2,11 @@ package com.development.SpringOAuthApplication.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
